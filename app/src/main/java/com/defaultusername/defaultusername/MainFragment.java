992f939920342efd4ab79e64953e9e69188181aa
JackieChan0 <jogarcia14@sfsharks.net>
2016-11-28 21:10:44 -0800

    Update MainFragment.java

--- app/src/main/java/com/defaultusername/defaultusername/MainFragment.java
+++ app/src/main/java/com/defaultusername/defaultusername/MainFragment.java
@@ -1,3 +1,15 @@
+/**************************************
+* file: MainFragment.java
+* @author Ervin Maulas, Joel Woods, Jose Garcia, Alan Chen
+* Class: CS 245 - Graphical User Interface
+*
+* Assignment: Android Memory Game
+* @version v1.0
+* Date Last Modified: 28 November 2016
+* Purpose: contains methods needed to utilize
+* the music for the game
+***************************************/
+
 public class MainFragment extends Fragment
 {
 	private AudioPlayer mPlayer = new AudioPlayer();
